19379fa219f4de6b77e4f1c9944ac39899c19301
dhubbard <dhubbard@oath.com>
2019-06-06 10:46:26 -0700

    Add skips to not run tests that require running from the repo directory when they are run from a temp directory which happens during the test_pypi_package install and test step.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import unittest
 from pypirun import cli
@@ -12,6 +13,7 @@ class TestClass(unittest.TestCase):
         super().tearDown()
         sys.argv = self._argv
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__install_and_run(self):
         rc = cli.install_and_run(package='.', command='pypirun_true', interpreter=cli.interpreter_parent(sys.executable))
         self.assertEqual(rc, 0)
@@ -20,26 +22,32 @@ class TestClass(unittest.TestCase):
         sys.argv = ['pypirun']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__already_installed(self):
         sys.argv = ['pypirun', '--debug', '.', 'pypirun']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__no_cache_dir(self):
         sys.argv = ['pypirun', '--no-cache-dir', '.', 'pypirun']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__upgrade_setuptools(self):
         sys.argv = ['pypirun', '--upgrade_setuptools', '.', 'pypirun']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__forced_install(self):
         sys.argv = ['pypirun', '--debug', '--always_install', '.', 'pypirun']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__invalid_command(self):
         sys.argv = ['pypirun', '--debug', '--always_install', '.', 'pypirun', 'fabzkkkks']
         self.assertEqual(cli.main(), 1)
 
+    @unittest.skipIf(not os.path.exists('setup.py'), "Not in package directory, no package to test against")
     def test__run__good_return(self):
         sys.argv = ['pypirun', '--debug', '--always_install', '.', 'pypirun_true']
         self.assertEqual(cli.main(), 0)
