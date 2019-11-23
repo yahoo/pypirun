98a29594179e67cb60635ab0d77e228dd6818f2e
Dwight Hubbard <254983+dwighthubbard@users.noreply.github.com>
2019-11-22 18:34:28 -0800

    Create __main__.py

---
+++ src/pypirun/__main__.py
@@ -0,0 +1,9 @@
+# Copyright 2019, Oath Inc.
+# Licensed under the terms of the BSD 3 Clause license. See LICENSE file in project root for terms.
+"""
+pypirun module entrypoint
+"""
+from .cli import main
+
+
+main()
